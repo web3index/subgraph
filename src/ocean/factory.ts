b0341aad71c02584eee19e922d51db774c962329
shreyas <spapinwar@gmail.com>
2021-08-17 15:44:23 +0530

    Added abi for factory contract on ocean

--- src/ocean/factory.ts
+++ src/ocean/factory.ts
@@ -1,5 +1,5 @@
 import { BPoolRegistered } from '../types/Factory/Factory'
-import { Pool as PoolContract } from '../types/templates/ocean/Pool'
+import { ocean as PoolContract } from '../types/templates'
 
 export function handleNewPool(event: BPoolRegistered): void {
   PoolContract.create(event.params.bpoolAddress)
